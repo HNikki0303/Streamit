cef9e6ce831c17cc44becc2d8dcf2ab7cdc3edac
HNikki0303 <nikitapant496@gmail.com>
2025-07-04 14:47:46 +0530

    User file and not EntryPoint

--- src/Components/User.jsx
+++ src/Components/User.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
 
-function EntryPoint() {
+function User() {
     return (
         <>
-            <div id="header" className='bg-[#E95670] h-[33vh] w-full '>
-            </div>
-            <div className='bg-[#B34270] h-45'></div>
-            <div className='bg-[#713770] h-37'></div>
-
+            <div>Hii this is Nikita user </div>
         </>
     )
 }
 
-export default EntryPoint;
+export default User;
