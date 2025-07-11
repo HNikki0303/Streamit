e240e51bf3cc3313d8421d164242b41163695550
HNikki0303 <nikitapant496@gmail.com>
2025-07-11 11:56:15 +0530

    navigation changed after Login or Register to PublishVideo Component

--- src/Components/LandingPage.jsx
+++ src/Components/LandingPage.jsx
@@ -29,7 +29,7 @@ export default function LandingPage() {
   const [register,showRegister]= useState(false);
 
   const handleSuccessfulRegistration=()=>{
-    navigate('/upload');
+    navigate('/publish');
   }
 
   return (
