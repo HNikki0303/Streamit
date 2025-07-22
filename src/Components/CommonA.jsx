52a826cbbaccb67b59d2315c886062b55fff948b
HNikki0303 <nikitapant496@gmail.com>
2025-07-22 21:24:53 +0530

    added more props to commonA

--- src/Components/CommonA.jsx
+++ src/Components/CommonA.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const CommonA = ({ children, userDetails, setShowAssistant }) => {
+const CommonA = ({ children, userDetails, showAssistant , setShowAssistant }) => {
   return (
     <div className="relative min-h-screen bg-gradient-to-br from-[#432F70] via-[#5E3B73] to-[#713770] text-white font-sans overflow-hidden">
       {/* Background Blobs */}
@@ -14,7 +14,7 @@ const CommonA = ({ children, userDetails, setShowAssistant }) => {
       >
         <div className="absolute top-3 right-4 z-20">
           <img
-            onClick={() => setShowAssistant(true)}
+            onClick={() => setShowAssistant(!showAssistant)}
             src="/chatbot.png"
             alt="AI Assistant"
             className="w-20 h-20 rounded-full border border-[#E95670] hover:scale-105 transition cursor-pointer"
@@ -35,7 +35,7 @@ const CommonA = ({ children, userDetails, setShowAssistant }) => {
           <p className="text-sm text-pink-200">{userDetails.fullName}</p>
         </div>
       </div>
-
+         {/* /*this is now more usesful*/ }
       {/* Centered Form Box */}
       <div className="flex justify-center items-start overflow-y-auto h-[calc(100vh-12rem)] px-4 py-6 z-10 relative">
         <div className="bg-white/10 backdrop-blur-md rounded-xl p-6 shadow-md w-full max-w-xl min-h-[300px]">
