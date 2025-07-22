82e5630352a9b85a7890c86472123f6a937b5e71
HNikki0303 <nikitapant496@gmail.com>
2025-07-22 21:22:49 +0530

    made changes to import reusable components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 
 import './App.css'
 import React from 'react'
-import LandingPage from './Components/LandingPage'
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import LandingPage from './Components/LandingPage';
 import UploadVideo from "./Components/UploadVideo";
-import AI_Assistance from './Components/AI_Assistance';
 import FormattedVideo from './Components/FormattedVideo';
-import PublishVideo from './Components/publishVideo';
+import PublishVideo from './Components/PublishVideo'; 
 import PaginatedVideoFeed from './Components/PaginatedVideoFeed';
+import UserChannel from './Components/UserChannel';
+import CommonA from './Components/CommonA';
+import CommonB from './Components/CommonB';
+import UpdateProfile from './Components/UpdateProfile';
+import AI_Assistant  from './Components/AI_Assistant'
+import FullVideo from './Components/FullVideo';
+import Channel_Description from './Components/Channel_Description'
 
 function App() {
   return (
@@ -17,11 +25,16 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/upload" element={<UploadVideo />} />
         <Route path="/publish" element = {<PublishVideo/>}/>
-        <Route path="/AI" element={<AI_Assistance/>}/>
+        <Route path="/AI" element={<AI_Assistant/>}/>
         <Route path="/FormattedVideo" element={<FormattedVideo/>}/>
         <Route path="/PaginatedVideoFeed" element = {<PaginatedVideoFeed/>}/>
+        <Route path="/UserChannel" element={<UserChannel/>}/>
+        <Route path ="/A" element={<CommonA/>}></Route>
+        <Route path = "/B" element={<CommonB/>}></Route>
+        <Route path= "/UpdateProfile" element={<UpdateProfile/>}></Route>
+        <Route path = "/FullVideo" element ={<FullVideo></FullVideo>} ></Route>
+        <Route path ='/channel-description' element={<Channel_Description></Channel_Description>}></Route>
 
-        
       </Routes>
     </Router>
 
